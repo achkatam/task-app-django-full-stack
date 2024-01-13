19f051a8fe8210b29c0e91aa7d231fb3d83c255c
achkatam <agmatevv@gmail.com>
2024-01-13 13:40:25 -0500

    added simple front-end - shuold be refactored soon CORS allowed

--- djangoapp/backend/djangoapp/settings.py
+++ djangoapp/backend/djangoapp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'taskapp',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,10 +50,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'djangoapp.urls'
 
+CORS_ALLOW_ALL_ORIGINS = True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
--- djangoapp/backend/taskapp/serializers.py
+++ djangoapp/backend/taskapp/serializers.py
@@ -11,4 +11,4 @@ class EmployeeSerializer(serializers.ModelSerializer):
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ['__all__']
+        fields = '__all__'
